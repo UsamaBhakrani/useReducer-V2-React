fdde6414ee21de815f8c4d06731f561f0b063a12
Usama Bhakrani <usamabhakrani@gmail.com>
2023-08-10 21:04:40 +0500

    replacing usestate with usereducer

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
+import reducer from "./reducer";
 
 const Counter = ({ initialCount }) => {
   const [count, setCount] = useState(initialCount);
   const [numberToAdd, setNumberToAdd] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCount(count + numberToAdd);
+    setNumberToAdd(0);
+  };
+
+  const handleOnChange = (e) => {
+    setNumberToAdd(parseInt(e.target.value));
+  };
+
   return (
     <>
       <h1>Count is {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setCount(count + numberToAdd);
-          setNumberToAdd(0);
-        }}
-      >
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="number">Add Alot</label>
         <input
           value={numberToAdd || ""}
           id="number"
           type="number"
-          onChange={(e) => setNumberToAdd(parseInt(e.target.value))}
+          onChange={handleOnChange}
         />
         <button>Add it</button>
       </form>
