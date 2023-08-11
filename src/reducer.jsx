48faf28018304e04d7a3d05a776879ae49f4c8c6
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-11 11:33:33 +0500

    using reducer logic with switch/case

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,20 +1,48 @@
 import { INCREMENT, DECREMENT, NUMBER_TO_ADD, ADD_FORM_INPUT } from "./actions";
 
 const reducer = (state, action) => {
-  if (action.type === INCREMENT) {
-    return { ...state, count: state.count + 1 };
-  }
-  if (action.type === DECREMENT) {
-    return { ...state, count: state.count - 1 };
-  }
-  if (action.type === NUMBER_TO_ADD) {
-    return { ...state, count: state.count + state.numberToAdd };
-  }
-  if (action.type === ADD_FORM_INPUT) {
-    return { ...state, numberToAdd: action.payload };
-  }
+  // If Else Logic for Reducer
 
-  return state;
+  // if (action.type === INCREMENT) {
+  //   return { ...state, count: state.count + 1 };
+  // }
+  // if (action.type === DECREMENT) {
+  //   return { ...state, count: state.count - 1 };
+  // }
+  // if (action.type === NUMBER_TO_ADD) {
+  //   return { ...state, count: state.count + state.numberToAdd };
+  // }
+  // if (action.type === ADD_FORM_INPUT) {
+  //   return { ...state, numberToAdd: action.payload };
+  // }
+
+  // return state;
+
+  // Switch/Case Logic for Reducer
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case NUMBER_TO_ADD:
+      return {
+        ...state,
+        count: state.count + state.numberToAdd,
+      };
+    case ADD_FORM_INPUT:
+      return {
+        ...state,
+        numberToAdd: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
